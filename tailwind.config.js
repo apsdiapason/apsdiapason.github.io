ba8648d4f6f2d4b5b48fb3fe6843d0bae510cb02
fabrisd <dvd.fabris@gmail.com>
2020-10-28 09:02:27 +0100

    fadein

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 2s ease-in forwards"
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 }
+        }
+      }
+    },
     fontFamily: {}
   },
-  variants: {},
+  variants: {
+    animation: ["motion-safe"]
+  },
   plugins: [],
   future: {
     removeDeprecatedGapUtilities: true,
